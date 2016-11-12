526f7c16cf4c5175fc53b6dfb8788292f3bc4674
Émile Plourde-Lavoie <nostradamus1935@echosvirtuels.com>
2016-11-12 16:51:25 -0500

    Add cycle option to i3 backgrounds

--- .i3/random_wallpaper.sh
+++ .i3/random_wallpaper.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-WALLPAPERS_FOLDER=/home/emile/.local/share/wallpapers 
+WALLPAPERS_FOLDER=/home/emile/.local/share/wallpapers
 
-WALLPAPERS=($(ls $WALLPAPERS_FOLDER -F))
-WP_COUNT=$(ls $WALLPAPERS_FOLDER -F | wc -l)
+WALLPAPERS=($(ls $WALLPAPERS_FOLDER -F | grep -v current))
+WP_COUNT=$(ls $WALLPAPERS_FOLDER -F | grep -v current | wc -l)
 
-feh --bg-scale $WALLPAPERS_FOLDER/${WALLPAPERS[ $RANDOM % $WP_COUNT ]}
+if [[ $1 = "next" ]]; then
+	NEXT_INDEX=$(expr $(cat $WALLPAPERS_FOLDER/current) + 1)
+	if [ "$NEXT_INDEX" -ge "$WP_COUNT" ]; then
+		NEXT_INDEX=0
+	fi
+	echo $NEXT_INDEX
+	feh --bg-scale $WALLPAPERS_FOLDER/${WALLPAPERS[ $NEXT_INDEX ]}
+	echo $NEXT_INDEX > $WALLPAPERS_FOLDER/current
+else
+	CURRENT_INDEX=$(expr $RANDOM % $WP_COUNT)
+	echo $CURRENT_INDEX > $WALLPAPERS_FOLDER/current
+	feh --bg-scale $WALLPAPERS_FOLDER/${WALLPAPERS[ $CURRENT_INDEX ]}
+fi
