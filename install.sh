9aec65e61feb51737770461ee486941a51b71fc0
Émile Plourde-Lavoie <emile@echosvirtuels.com>
2023-07-17 11:49:43 -0400

    Add some extra tools install steps to install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ DIRNAME=$(dirname "$0")
 TMP_ROOT=$(mktemp -d "/tmp/${BASENAME}.XXXXXX")
 trap 'rm -rf $TMP_ROOT' EXIT
 
-echo "Installing this repositories file in the home directory"
+echo "Installing files in the home directory"
 
 find . -type f ! -path '*.git*' ! -name README.md ! -name install.sh ! -name sync.sh  -printf '%P\n' > $TMP_ROOT/filelist
 
@@ -58,3 +58,32 @@ do
 		fi
 	fi
 done
+
+echo "Installing tools..."
+
+if test -f /etc/issue.net && cat /etc/issue.net | grep Debian > /dev/null
+then
+	sudo apt install build-essential cmake python-is-python3 vim-gtk3
+fi
+
+echo " → Rust"
+
+if ! test -d ~/.rbenv
+then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+cargo install du-dust
+cargo install bat
+cargo install exa
+
+echo " ✓ Done."
+
+echo " → Rbenv"
+
+if ! test -d ~/.rbenv
+then
+	git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+fi
+
+echo " ✓ Done."
