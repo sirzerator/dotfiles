4576627fb4aef4f8b802acc5deb534e14e5e5680
Émile Plourde-Lavoie <emile@echosvirtuels.com>
2024-06-09 20:22:04 -0400

    Add PHP install section

--- install.sh
+++ install.sh
@@ -81,6 +81,22 @@ fi
 
 echo " ✓ Done."
 
+echo " → PHP"
+
+if ! command -v php > /dev/null
+then
+	sudo apt-get update
+	sudo apt-get -y install lsb-release ca-certificates curl
+	sudo curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+	sudo dpkg -i /tmp/debsuryorg-archive-keyring.deb
+	sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+	sudo rm /tmp/debsuryorg-archive-keyring.deb
+	sudo apt-get update
+	sudo apt install php-cli
+fi
+
+echo " ✓ Done."
+
 echo " → Composer"
 
 if ! command -v composer > /dev/null
